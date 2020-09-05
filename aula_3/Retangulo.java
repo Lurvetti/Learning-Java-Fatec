1eaeb2cb554c41b87f510824f6542026f2266644
Lurvetti <lurvetti@gmail.com>
2020-09-04 21:50:34 -0300

    go

--- aula_3/Retangulo.java
+++ aula_3/Retangulo.java
@@ -1,16 +1,25 @@
 public class Retangulo {
 
-    //atributos
+    // atributos
     double base;
     double altura;
     String cor;
 
-    //métodos
+    // métodos
     public double calcularArea(){
         return base * altura;
     }
 
     public double calcularPerimetro(){
         return 2 * (base + altura);
+    }
+
+    public void imprimirDados(){
+        System.out.println();
+        System.out.println(base);
+        System.out.println(altura);
+        System.out.println(cor);
+        System.out.println("Area: " + calcularArea());
+        System.out.println("Perimetro: " + calcularPerimetro());
     }
 }
--- aula_3/TestaRetangulo.java
+++ aula_3/TestaRetangulo.java
@@ -1,11 +1,17 @@
 public class TestaRetangulo {
     public static void main(String[] args) {
         Retangulo ret01 = new Retangulo();
-        System.out.println(ret01);
-
+        Retangulo ret02 = new Retangulo();
+        
         ret01.altura = 5;
         ret01.base = 10;
+        ret01.cor = "Vermelho";
+
+        ret02.altura = 20;
+        ret02.base = 30;
+        ret02.cor = "Amarelo";
         
-        System.out.println(ret01.calcularArea());
+        ret01.imprimirDados();
+        ret02.imprimirDados();
     }
 }
